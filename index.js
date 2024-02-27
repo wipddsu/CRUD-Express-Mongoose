2fd1e7f11e33fcdc943500e6864fbd96635b6c27
wipddsu <rlaalstn8158@naver.com>
2024-02-27 22:07:38 +0900

    POST - Add a new product

--- index.js
+++ index.js
@@ -13,16 +13,32 @@ main().catch((err) => {
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/farmStand');
   console.log('MONGO CONNECTION OPEN!!!');
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
+app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/products', async (req, res) => {
   const products = await Product.find({});
   res.render('products/index', { products });
+});
+
+app.get('/products/new', (req, res) => {
+  res.render('products/new');
+});
+
+app.post('/products', async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  console.log(newProduct);
+  // const { name, price, category } = req.body;
+  // Product.insertMany({
+  //   name,
+  //   price,
+  //   category,
+  // });
+  res.redirect(`/products/${newProduct._id}`);
 });
 
 app.get('/products/:id', async (req, res) => {
