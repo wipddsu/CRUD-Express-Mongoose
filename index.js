f964356ed4a1cc4d53f4ae103b724b5fe2c50fb2
wipddsu <rlaalstn8158@naver.com>
2024-02-28 12:21:08 +0900

    Change 'radio' code to ejs syntax

--- index.js
+++ index.js
@@ -22,13 +22,15 @@ app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const categories = ['fruit', 'vegetable', 'dairy'];
+
 app.get('/products', async (req, res) => {
   const products = await Product.find({});
   res.render('products/index', { products });
 });
 
 app.get('/products/new', (req, res) => {
-  res.render('products/new');
+  res.render('products/new', { categories });
 });
 
 app.post('/products', async (req, res) => {
@@ -52,7 +54,7 @@ app.get('/products/:id', async (req, res) => {
 app.get('/products/:id/edit', async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
-  res.render('products/edit', { product });
+  res.render('products/edit', { product, categories });
 });
 
 app.put('/products/:id', async (req, res) => {
