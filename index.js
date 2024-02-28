94e3bfd6097e3926e6b5c89a02ce6dd2da90f7d2
wipddsu <rlaalstn8158@naver.com>
2024-02-28 12:38:47 +0900

    Delete - Delete a product

--- index.js
+++ index.js
@@ -63,6 +63,12 @@ app.put('/products/:id', async (req, res) => {
   res.redirect(`/products/${product._id}`);
 });
 
+app.delete('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  await Product.findByIdAndDelete(id);
+  res.redirect(`/products`);
+});
+
 app.listen(3000, () => {
   console.log('APP IS LISTENING ON PORT 3000');
 });
